4b9fa58add07b738d90971c925d2969b90c44429
sagnikcapital <sagnikd@capitalnumbers.com>
2024-02-15 13:33:08 +0530

    read-md

--- src/Classes/Folder.php
+++ src/Classes/Folder.php
@@ -3,5 +3,36 @@
 namespace Sagnikd\FolderAspire;
 
 class Folder {
+  /**
+   * Constructor
+   */
+  public function __construct()
+  {
+
+  }
   
+  /**
+   * Create directory
+   *
+   * @param [type] $folderName
+   * @param [type] $permission
+   * @return void
+   */
+  public function createDirectory($folderName, $permission)
+  {
+
+  }
+
+  /**
+   * Lock or Abandoned folder
+   *
+   * @param [type] $folderName
+   * @param string $lockType
+   * @param [type] $path
+   * @return void
+   */
+  public function lockDirectory($folderName,  $lockType = 'exclusive', $path)
+  {
+
+  }
 }

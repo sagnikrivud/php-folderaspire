3841d9696d58ef94324d252ce05b6d1d4234af85
sagnikcapital <sagnikd@capitalnumbers.com>
2024-02-19 10:59:00 +0530

    error-add

--- lang/en/error.php
+++ lang/en/error.php
@@ -1,4 +1,8 @@
 <?php
 return [
-  'error_unable_to_read' => 'The provided file of {path} path is not readable.'
+  'php_version_error' => 'Minimum PHP should be 8.0 or above',
+  'error_unable_to_read' => 'The provided file of {path} path is not readable.',
+  'directory_error' => 'This is not an directory',
+  'directory_not_found' => 'Unable to import directory',
+  'permission_error' => 'Please check the required permission of apache'
 ];
